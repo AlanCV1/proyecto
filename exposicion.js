921b04f0808a770e129f0ee0533d6e19d3a5ed24
AlanCV1 <alanvences711@gmail.com>
2025-03-18 09:30:28 -0600

    Add files via upload

--- exposicion.js
+++ exposicion.js
@@ -1,47 +1,48 @@
 //Formas de crear un arreglo
 
-/*let numeros = [10, 20, 30, 40, 50]
-console.log(numeros)*/
+let numeros = [10, 20, 30, 40, 50]
+console.log(numeros)
 
-/*let planetas = new Array("Marte", "Jupiter", "Saturno")
+//let planetas = new Array("Marte", "Jupiter", "Saturno")
 console.log(planetas)
 
 //Acceder a los elementos de un arreglo
 console.log("Primer planeta:", planetas[0]);
-console.log("Último planeta:", planetas[planetas.length - 1]);*/
+console.log("Último planeta:", planetas[planetas.length - 1]);
 
 
 //Métodos comunes de los arreglos
 let planetas = ["Venus", "Tierra", "Marte", "Jupiter"];
 
 // Agregar elementos
-/*planetas.push("Saturno")  // Agrega al final
-console.log("Después de push:", planetas)*/
+planetas.push("Saturno")  // Agrega al final
+console.log("Después de push:", planetas)
 
 // Eliminar elementos
 planetas.pop()  // Elimina el último
 console.log("Después de pop:", planetas)
 
 
-/*planetas.unshift("Mercurio") // Agrega al inicio
-console.log("Después de unshift:", planetas)*/
+planetas.unshift("Mercurio") // Agrega al inicio
+console.log("Después de unshift:", planetas)
  
 
-/*planetas.shift() // Elimina el primero
-console.log("Después de shift:", planetas)*/
+planetas.shift() // Elimina el primero
+console.log("Después de shift:", planetas)
 
 //Propiedades útiles de los arreglos
-/*console.log("Número de planetas:", planetas.length)*/
-/*console.log("¿El arreglo incluye a 'Venus'?", planetas.includes("venus"),"si lo incluye")
+console.log("Número de planetas:", planetas.length)
+console.log("¿El arreglo incluye a 'Venus'?", planetas.includes("venus"),"si lo incluye")
 
 
 //Ordenar un arreglo
-/*let numeros = [3, 4, 1, 5, 7, 2, 6]
+
+let numeros = [3, 4, 1, 5, 7, 2, 6]
 numeros.sort(); // Ordena de menor a mayor (como texto)
 console.log("Ordenados:", numeros)
 
 numeros.reverse(); // Invierte el orden
-console.log("Invertidos:", numeros)*/
+console.log("Invertidos:", numeros)
 
 //Arreglos multidimensionales
 let matriz = [
